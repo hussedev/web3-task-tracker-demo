35405f6e860a1349e885a41ee4d68a67db3047d7
Hussein Martinez <husse.dev@gmail.com>
2024-04-21 19:23:10 +0700

    created components/index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Footer } from "./Footer";
+export { Header } from "./Header";
+export { NavBar } from "./NavBar";
+export { TaskList } from "./TaskList";
+export { ProjectDescription } from "./ProjectDescription";
