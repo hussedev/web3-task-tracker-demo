ac4fc23a759a47a4820466c8a2b7aa797c546347
Hussein Martinez <husse.dev@gmail.com>
2024-04-21 19:21:09 +0700

    implemented ProjectDescription

---
+++ src/components/ProjectDescription.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+export const ProjectDescription: FC = () => {
+  return (
+    <div
+      style={{
+        margin: "2rem",
+        padding: "1rem",
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        backgroundColor: "#f9f9f9"
+      }}
+    >
+      <p>
+        This is a simple app to compare smart contract implementations in different blockchains.
+      </p>
+      <p>
+        The goal is to provide a platform for developers to easily compare and contrast the features
+        and capabilities of different blockchain platforms.
+      </p>
+      <p>To begin exploring, select a blockchain:</p>
+      <ul>
+        <li>
+          <Link to="/ethereum">Ethereum</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
