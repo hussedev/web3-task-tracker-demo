2b7de897068d38c0ec6acadc28e8cb48316875fc
Hussein Martinez <husse.dev@gmail.com>
2024-04-27 12:00:29 +0700

    moved lib/ethereum/truffle to lib/truffle

--- src/lib/ethereum/config.ts
+++ src/lib/ethereum/config.ts
@@ -1,4 +1,4 @@
-import contractData from "./truffle/build/TaskContract.json";
+import contractData from "../truffle/build/TaskContract.json";
 
 export const contractABI = contractData.abi;
 export const contractAddress = "CONTRACT ADDRESS";
