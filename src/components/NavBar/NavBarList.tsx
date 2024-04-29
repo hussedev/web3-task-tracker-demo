287698a05cf87297576f0ff38a4f58fcbba81b55
Hussein Martinez <husse.dev@gmail.com>
2024-04-30 03:31:34 +0700

    refactor routes and NavBarList

--- src/components/NavBar/NavBarList.tsx
+++ src/components/NavBar/NavBarList.tsx
@@ -2,18 +2,13 @@ import { FC } from "react";
 import { useLocation } from "react-router-dom";
 import { NavBarItem } from "./NavBarItem";
 import { navListStyle } from "./styles";
-
-const items = [
-  { title: "Home", path: "/" },
-  { title: "Eth-Ethers", path: "/eth-ethers" },
-  { title: "Eth-wagmi", path: "/eth-wagmi" }
-];
+import { appChildren } from "@/routes";
 
 export const NavBarList: FC = () => {
   const { pathname } = useLocation();
   return (
     <ul style={navListStyle}>
-      {items.map(({ title, path }) => (
+      {appChildren.map(({ title, path }) => (
         <NavBarItem key={path} title={title} path={path} isSelected={pathname === path} />
       ))}
     </ul>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,26 @@
 import { App } from "./App";
-import { Main } from "./pages";
-import { EthereumTasks } from "./features/ethereum";
+import { EthersTasks } from "@features/ethers";
+import { Main } from "@pages";
+import { ReactNode } from "react";
+import { Route, createRoutesFromElements } from "react-router-dom";
 
-export const routes = [
+export const appChildren: Array<{ title: string; path: string; element: ReactNode }> = [
   {
+    title: "Home",
     path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Main />
-      },
-      {
-        path: "/ethereum",
-        element: <EthereumTasks />
-      }
-    ]
+    element: <Main />
+  },
+  {
+    title: "Ethers",
+    path: "/eth-ethers",
+    element: <EthersTasks />
   }
 ];
+
+export const routes = createRoutesFromElements(
+  <Route path="/" element={<App />}>
+    {appChildren.map(({ path, element }) => (
+      <Route key={path} path={path} element={element} />
+    ))}
+  </Route>
+);
